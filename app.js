263b38759dcb73abff56193187e42d5121368461
Julian Gruber <julian@juliangruber.com>
2016-06-01 13:02:40 +0200

    move replication logic to lib

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const level = require('level');
 const hyperdrive = require('hyperdrive');
-const webRTCSwarm = require('webrtc-swarm');
-const discoverySwarm = require('discovery-swarm');
-const swarmDefaults = require('datland-swarm-defaults');
-const signalhub = require('signalhub');
 const app = require('electron').remote.app;
 const {ipcRenderer: ipc} = require('electron');
 const drop = require('drag-and-drop-files');
@@ -11,6 +7,7 @@ const fileReader = require('filereader-stream');
 const fs = require('fs');
 const {basename} = require('path');
 const raf = require('random-access-file');
+const replicate = require('./lib/replicate');
 
 const appPath = `${app.getPath('appData')}/${app.getName()}`;
 const filesPath = `${app.getPath('downloads')}/dat`;
@@ -29,18 +26,7 @@ const archive = drive.createArchive(key, {
 });
 fs.writeFileSync(keyPath, archive.key);
 
-const swarmKey = `dat-desktop-${archive.key.toString('hex')}`;
-
-const ws = webRTCSwarm(signalhub(swarmKey, 'https://signalhub.mafintosh.com'));
-ws.on('peer', peer => {
-  peer.pipe(archive.replicate()).pipe(peer);
-});
-
-const ds = discoverySwarm(swarmDefaults({
-  stream: () => archive.replicate()
-}));
-ds.once('listening', () => ds.join(swarmKey));
-ds.listen(3282);
+replicate(archive);
 
 archive.list({ live: true }).on('data', entry => {
   document.body.innerHTML += entry.name + '<br>';
