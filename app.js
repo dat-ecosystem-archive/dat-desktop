b9f6c0417e67e23db077a38c4a876a8a9c6b99e4
Julian Gruber <julian@juliangruber.com>
2016-06-01 13:37:27 +0200

    add ui for adding archives

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const level = require('level');
 const hyperdrive = require('hyperdrive');
-const app = require('electron').remote.app;
+const {app, dialog} = require('electron').remote;
 const {ipcRenderer: ipc} = require('electron');
 const drop = require('drag-and-drop-files');
 const fileReader = require('filereader-stream');
@@ -38,7 +38,13 @@ replicate(local);
 const archives = new Set;
 let el;
 
-const render = archives => yo`
+const addArchive = ev => {
+  ev.preventDefault();
+  const link = ev.target.querySelector('input').value;
+  db.put(['archive', link], link);
+};
+
+const render = (archives, add) => yo`
   <div>
     <h2>Archives</h2>
     <ul>
@@ -47,11 +53,15 @@ const render = archives => yo`
         <li>${key.toString('hex')}</li>
       `)}
     </ul>
+    <form onsubmit=${add}>
+      <input type="text" placeholder="Link">
+      <input type="submit" value="Add archive">
+    </form>
   </div>
 `;
 
 const refresh = () => {
-  el = yo.update(el, render(archives));
+  el = yo.update(el, render(archives, addArchive));
 };
 
 liveStream(db, {
@@ -64,7 +74,7 @@ liveStream(db, {
   refresh();
 });
 
-el = render(archives);
+el = render(archives, addArchive);
 document.body.appendChild(el);
 
 
