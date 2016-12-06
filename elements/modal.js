0d595c167a91f75f394c28256b47a30c8c9cccbc
Kristina Schneider <me@kristinaschneider.com>
2016-12-07 09:17:27 +1100

    style modal (in progress)

--- elements/modal.js
+++ elements/modal.js
@@ -1,6 +1,46 @@
 const widget = require('cache-element/widget')
 const Modal = require('../lib/modal-element')
 const html = require('choo/html')
+var css = require('sheetify')
+
+css('dat-colors')
+
+var prefix = css`
+  :host {
+    box-shadow: 0 1.2rem 2.4rem rgba(0,0,0,.5);
+    position: relative;
+  }
+  .exit {
+    width: 2rem;
+    height: 2rem;
+    padding: 0;
+    position: absolute;
+    top: 0;
+    right: 0;
+    background: transparent;
+    border: none;
+    text-align: center;
+    color: var(--color-neutral-40);
+  }
+  .exit:hover, .exit:focus {
+    color: var(--color-neutral);
+  }
+  .exit-svg {
+    fill: currentColor;
+    vertical-align: middle;
+    width: 1.1em;
+    max-height: 1.6em;
+  }
+  .modal-input {
+    height: 2rem;
+    padding-right: .5rem;
+    padding-left: 2rem;
+    font-size: .875rem;
+    border: 1px solid var(--color-neutral-30);
+    background-color: transparent;
+    color: var(--color-green-hover);
+  }
+`
 
 module.exports = createWidget
 
@@ -48,14 +88,22 @@ function createWidget () {
 
   function createHtml (link) {
     return html`
-      <section class="flex flex-column items-center justify-center pa3 b--solid bg-white">
-        <h3>Your dat link:</h3>
-        <input type="text" value=${link}/>
-        <p>
-          This link has also been copied to the clipboard for your
-          convenience.
+      <section class="${prefix} flex flex-column items-center justify-center pa3 bg-white">
+        <h3>Copy Dat Link</h3>
+        <label for="dat-link" class="dat-import">
+          <input name="dat-link" type="text" placeholder="Import dat" value=${link} class="modal-input">
+          <svg>
+            <use xlink:href="#daticon-link" />
+          </svg>
+        </label>
+        <p class="f7">
+          Anyone with this link can view your Dat.
         </p>
-        <button onclick=${handleExit} class="pointer">Ok cool</button>
+        <button onclick=${handleExit} class="pointer exit" aria-label="Close">
+          <svg class="exit-svg">
+            <use xlink:href="#daticon-cross" />
+          </svg>
+        </button>
       </section>
     `
 
