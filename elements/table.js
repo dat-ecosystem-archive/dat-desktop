2a9c47a0624521edd724441b517c7e673115ee06
Kristina Schneider <me@kristinaschneider.com>
2016-12-10 14:05:43 +1100

    move progress bar styles into table.js

--- elements/table.js
+++ elements/table.js
@@ -97,6 +97,86 @@ const table = css`
   }
 `
 
+const progressBar = css`
+  :host {
+    --progress-height: .5rem;
+    --bar-height: var(--progress-height);
+    --counter-width: 2.5rem;
+
+    min-width: 8rem;
+    overflow: hidden;
+    padding-top: .85rem;
+    padding-bottom: .5rem;
+  }
+
+  .bar {
+    height: var(--progress-height);
+    width: calc(100% - var(--counter-width));
+    float: left;
+    overflow: hidden;
+    background-color: var(--color-neutral-20);
+    border-radius: 2px;
+  }
+
+  .line {
+    width: 0%;
+    height: var(--progress-height);
+    background-color: var(--color-blue);
+    border-radius: 2px;
+  }
+
+  @keyframes move-bg {
+    0% {
+      background-position: var(--tile-width) 0;
+    }
+    100% {
+      background-position: 0 0;
+    }
+  }
+
+  .line-loading {
+    --tile-width: 28px;
+    --stripe-width: 5px;
+
+    overflow: hidden;
+    position: relative;
+    height: var(--bar-height);
+  }
+  .line-loading:before {
+    content: '';
+    width: 100%;
+    height: var(--bar-height);
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-image: repeating-linear-gradient(
+      -45deg,
+      transparent,
+      transparent var(--stripe-width),
+      rgba(255,255,255,.1) var(--stripe-width),
+      rgba(255,255,255,.1) calc(2 * var(--stripe-width))
+    );
+    background-size: var(--tile-width) var(--bar-height);
+    animation: move-bg .75s linear infinite;
+  }
+
+  .line-complete {
+    background-color: var(--color-green);
+  }
+
+  .line-paused {
+    background-color: var(--color-neutral-60);
+  }
+
+  .counter {
+    float: right;
+    min-width: var(--counter-width);
+    margin-top: -.4rem;
+    text-align: right;
+    font-size: .875rem;
+  }
+`
+
 module.exports = tableElement
 
 function tableElement (dats, send) {
@@ -162,13 +242,13 @@ function createTable (dats, send) {
           </div>
         </td>
         <td class="cell-3">
-          <div class="progress">
-            <div class="progress__counter">
+          <div class="${progressBar}">
+            <div class="counter">
               ${progress}%
             </div>
-            <div class="progress__bar">
+            <div class="bar">
               <div
-                class="progress__line progress__line--${state}"
+                class="line line-${state}"
                 style="width: ${progress}%"
               ></div>
             </div>
