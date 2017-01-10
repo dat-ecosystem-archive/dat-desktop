567b765ce3b80bc118ad200c8d81a57ebe6ff28f
Julian Gruber <julian@juliangruber.com>
2017-01-10 15:52:54 +0100

    add automatic check for updates and install

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const defaultMenu = require('electron-default-menu')
-const { app, shell, Menu, ipcMain } = require('electron')
+const { app, shell, Menu, ipcMain, dialog } = require('electron')
 const window = require('electron-window')
 const Env = require('envobj')
 const path = require('path')
 const doctor = require('dat-doctor')
 const { Writable } = require('stream')
+const { autoUpdater } = require('electron-auto-updater')
+const os = require('os')
 
 const delegateEvents = require('./lib/delegate-electron-events')
 
@@ -45,6 +47,33 @@ app.on('ready', () => {
     Menu.setApplicationMenu(Menu.buildFromTemplate(menu))
     if (env.NODE_ENV === 'development') {
       mainWindow.webContents.openDevTools({ mode: 'detach' })
+    } else {
+      const platform = `${os.platform()}_${os.arch()}`
+      const version = app.getVersion()
+      const log = str => mainWindow.webContents.send('log', str)
+      autoUpdater.setFeedURL(`http://dat.land:6000/update/${platform}/${version}`)
+      autoUpdater.on('error', err => log(err.stack))
+      autoUpdater.on('checking-for-update', () => log('checking for update'))
+      autoUpdater.on('update-available', () => log('update available'))
+      autoUpdater.on('update-not-available', () => log('update not available'))
+      autoUpdater.on('download-progress', p => log('download progress ' + p.percent))
+      autoUpdater.on('update-downloaded', (ev, notes, name) => {
+        log('update downloaded')
+        dialog.showMessageBox(mainWindow, {
+          type: 'question',
+          buttons: ['Install and Relaunch', 'Skip Update'],
+          defaultId: 0,
+          title: 'A new version of Dat Desktop is ready to install!',
+          message: `Dat Desktop ${name} has been downloaded and is ready to use! Would you like to install it and relaunch Dat Desktop now?`
+        }, res => {
+          if (res === 0) {
+            log('updating...')
+            autoUpdater.quitAndInstall()
+            app.quit()
+          }
+        })
+      })
+      autoUpdater.checkForUpdates()
     }
   })
 })
