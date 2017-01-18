a5d4ce1878ae2a6831faa19b7933e2353058bb36
Julian Gruber <julian@juliangruber.com>
2017-01-18 15:52:29 +0100

    Fix browserify bundling (#154)
    * refactor bundle script to use browserify api
    
    * refactor watch script to use api as well
    
    * switch back to npm version of browserify
    
    * get rid of bindings-browserify

---
+++ scripts/browserify.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+const browserify = require('browserify')
+const watchify = require('watchify')
+const fs = require('fs')
+const path = require('path')
+
+const watch = process.argv[2] === 'watch'
+
+const opts = {
+  insertGlobals: true,
+  ignoreMissing: true,
+  builtins: false,
+  browserField: false,
+  insertGlobalVars: {
+    '__dirname': (file, basedir) => {
+      return '__dirname + "/" + ' +
+        JSON.stringify(path.dirname(path.relative(basedir, file)))
+    },
+    '__filename': (file, basedir) => {
+      return '__filename + "/" + ' +
+        JSON.stringify(path.relative(basedir, file))
+    },
+    'process': undefined,
+    'global': undefined,
+    'Buffer': undefined,
+    'Buffer.isBuffer': undefined
+  }
+}
+
+if (watch) {
+  opts.cache = {}
+  opts.packageCache = {}
+  opts.plugin = [watchify]
+}
+
+const b = browserify(`${__dirname}/../app.js`, opts)
+
+b.exclude('electron')
+b.transform('bindingify', { global: true })
+b.transform('sheetify/transform', { use: ['sheetify-nested'] })
+
+const bundle = () => {
+  process.stderr.write(`${new Date()} bundling...`)
+  b.bundle().pipe(fs.createWriteStream(`${__dirname}/../bundle.js`))
+  .on('finish', () => process.stderr.write('ok!\n'))
+}
+
+b.on('update', bundle)
+bundle()
