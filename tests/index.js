53e82b6f65dacaa9b943ce514e432d1fa9055492
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-01-27 11:26:51 -0800

    add another test, use arrow functions

--- tests/index.js
+++ tests/index.js
@@ -3,23 +3,32 @@ var path = require('path')
 var tape = require('tape')
 
 tape('application test', function (t) {
-  t.test('should run the thing', function (t) {
-    var app = new spectron.Application({
-      path: path.join(__dirname, '../node_modules/.bin/electron'),
-      args: [path.join(__dirname, '../index.js')]
-    })
-    app.start().then(function () {
-      return app.browserWindow.isVisible()
-    }).then(function (isVisible) {
-      t.equal(isVisible, true)
-    }).then(function () {
-      return app.client.getTitle()
-    }).then(function (title) {
-      t.equal(title, 'Dat Desktop')
-    }).then(function () {
-      return app.stop()
-    }).catch(function (error) {
-      console.error('Test failed', error.message)
-    })
+  t.test('should be able to boot up the app', function (t) {
+    t.plan(2)
+    var app = createApp()
+    app.start()
+      .then(() => app.browserWindow.isVisible())
+      .then((isVisible) => t.equal(isVisible, true))
+      .then(() => app.stop())
+      .then(() => t.pass('app stopped'))
+      .catch((error) => console.error('Test failed', error.message))
+  })
+
+  t.test('welcome screen should appear, and be dismissable', function (t) {
+    t.plan(2)
+    var app = createApp()
+    app.start()
+      .then(() => app.browserWindow.isVisible())
+      .then((isVisible) => t.equal(isVisible, true))
+      .then(() => app.stop())
+      .then(() => t.pass('app stopped'))
+      .catch((error) => console.error('Test failed', error.message))
   })
 })
+
+function createApp () {
+  return new spectron.Application({
+    path: path.join(__dirname, '../node_modules/.bin/electron'),
+    args: [path.join(__dirname, '../index.js')]
+  })
+}
