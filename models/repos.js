50c37355fa2ff1c8a13254185dbee6beec722020
Julian Gruber <julian@juliangruber.com>
2017-02-14 14:35:09 +0100

    WIP dat-worker

--- models/repos.js
+++ models/repos.js
@@ -211,6 +211,13 @@ function createManager (multidat, onupdate) {
     assert.equal(typeof opts, 'object', 'models/repos: dat-manager: opts should be a object')
     assert.equal(typeof cb, 'function', 'models/repos: dat-manager: cb should be a function')
 
+    opts = Object.assign(opts, {
+      watch: true,
+      resume: true,
+      ignoreHidden: true,
+      compareFileContent: true
+    })
+
     multidat.create(dir, opts, function (err, dat) {
       if (err) return cb(err)
       initDat(dat)
@@ -233,27 +240,7 @@ function createManager (multidat, onupdate) {
   }
 
   function initDat (dat) {
-    var network = dat.joinNetwork()
-    dat.network = network
-
-    var stats = dat.trackStats()
-    dat.metadata = dat.metadata || {}
-    dat.stats = stats
-
-    if (dat.owner) {
-      var importer = dat.importFiles({
-        watch: true,
-        resume: true,
-        ignoreHidden: true,
-        compareFileContent: true
-      }, function (err) {
-        if (err) throw err
-        update()
-      })
-      importer.on('file imported', function () {
-        update()
-      })
-    }
+    dat.metadata = {}
 
     multidat.readManifest(dat, function (_, manifest) {
       if (!manifest) return
@@ -262,10 +249,6 @@ function createManager (multidat, onupdate) {
       update()
     })
 
-    dat.archive.on('download', update)
-    dat.network.on('connection', function (peer) {
-      update()
-      peer.once('close', update)
-    })
+    dat.on('update', update)
   }
 }
