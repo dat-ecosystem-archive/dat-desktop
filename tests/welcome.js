cbc2feb43b4643cd3a168e283175806f7dcf0a3b
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-03-28 20:46:00 +0200

    add unit tests

---
+++ tests/welcome.js
@@ -0,0 +1,54 @@
+var EventEmitter = require('events').EventEmitter
+var tape = require('tape')
+var spok = require('spok')
+
+var welcome = require('../models/welcome')
+
+tape('models/welcome: should initialize with a default state', function (t) {
+  var state = {}
+  var emitter = new EventEmitter()
+  welcome(state, emitter)
+  spok(t, state, {
+    welcome: {
+      show: false
+    }
+  })
+  t.end()
+})
+
+tape('should show welcome screen if there are no dats', function (t) {
+  var state = {
+    repos: {
+      values: []
+    },
+    welcome: {
+      show: true
+    }
+  }
+  var emitter = new EventEmitter()
+  welcome(state, emitter)
+  emitter.emit('dats:loaded')
+  spok(t, state, {
+    welcome: {
+      show: true
+    }
+  })
+  t.end()
+})
+
+tape('should be able to hide the welcome screen', function (t) {
+  var state = {
+    welcome: {
+      show: true
+    }
+  }
+  var emitter = new EventEmitter()
+  welcome(state, emitter)
+  emitter.emit('welcome:hide')
+  spok(t, state, {
+    welcome: {
+      show: false
+    }
+  })
+  t.end()
+})
