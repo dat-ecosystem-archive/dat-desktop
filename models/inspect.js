8a925f66a9bbea97849b1ba227d46397a1a93fd7
Julian Gruber <julian@juliangruber.com>
2017-05-25 10:46:52 +0200

    models/inspect: add .file walk

--- models/inspect.js
+++ models/inspect.js
@@ -1,3 +1,4 @@
+var join = require('path').join
 var xtend = Object.assign
 
 module.exports = inspectModel
@@ -12,8 +13,42 @@ function inspectModel (state, bus) {
   bus.on('dats:inspect', function (dat) {
     state.inspect.show = true
     state.inspect.dat = dat
+
     update()
     dat.stats.on('update', update)
+
+    if (!dat.files) {
+      dat.files = []
+
+      function sort () {
+        dat.files.sort(function (a, b) {
+          return a.path.localeCompare(b.path)
+        })
+      }
+
+      function walk (dir) {
+        dat.archive.readdir(dir, function (err, names) {
+          if (err) return
+          names.forEach(function (name) {
+            var file = { path: join(dir, name) }
+            dat.files.push(file)
+            sort()
+            dat.archive.stat(file.path, function (err, stat) {
+              if (err) return
+              file.stat = stat
+              if (stat.isDirectory()) {
+                file.path += '/'
+                walk(file.path)
+              }
+              sort()
+              update()
+            })
+          })
+          update()
+        })
+      }
+      walk('/')
+    }
   })
 
   bus.on('inspect:hide', function () {
