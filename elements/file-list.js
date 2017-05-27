26237a5df8f8dd53ba2ed547b567e670f87f955f
Julian Gruber <julian@juliangruber.com>
2017-05-27 15:53:07 +0200

    refactor file-list into element (#397)
    * refactor file-list into element
    
    later to be used by the inspect screen as well
    
    * remove new line (standard)

---
+++ elements/file-list.js
@@ -0,0 +1,53 @@
+var bytes = require('prettier-bytes')
+var html = require('choo/html')
+var css = require('sheetify')
+
+var fileListContainer = css`
+  :host {
+    min-height: 5rem;
+  }
+`
+
+var fileList = css`
+  :host {
+    td {
+      padding: .25rem .5rem;
+    }
+    tr:odd td {
+      background-color: var(--color-neutral-04);
+    }
+  }
+`
+
+module.exports = function (dat) {
+  return html`
+    <div class="flex-auto bg-white mb2 mw6 ${fileListContainer}">
+      ${dat && dat.files
+        ? html`
+          <table class="w-100 f7 f6-l ${fileList}">
+            ${dat.files.map(file => {
+              var size = file.stat && file.stat.isFile()
+                ? ` ${bytes(file.stat.size)}`
+                : ''
+              return html`
+                <tr>
+                  <td class="truncate mw5">
+                    ${file.path}
+                  </td>
+                  <td>
+                    ${size}
+                  </td>
+                </tr>
+              `
+            })}
+          </table>
+          `
+        : html`
+          <div class="f7 f6-l pa2">
+            N/A
+          </div>
+        `
+      }
+    </div>
+  `
+}
