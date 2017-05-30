7236d9a16bf44568415c363bec9c8a7cb4446dbf
Julian Gruber <julian@juliangruber.com>
2017-05-30 06:28:30 +0200

    Fix deleting error dats. Closes #400. (#405)
    * wip
    
    * cleanup
    
    * use error haxagon icon
    
    * more styles

--- elements/table-row.js
+++ elements/table-row.js
@@ -119,7 +119,7 @@ function Row ({ highlight }) {
 
   function render () {
     var { dat, state, emit } = this.props
-    if (dat instanceof Error) return errorRow(dat)
+    if (dat instanceof Error) return errorRow(dat, emit, deleteButton)
 
     var stats = dat.stats
     var peers = dat.network ? dat.network.connected : 'N/A'
@@ -244,7 +244,7 @@ function DeleteButton () {
       onclick: function (e) {
         e.preventDefault()
         e.stopPropagation()
-        emit('dats:remove', { key: dat.key })
+        emit('dats:remove', { key: dat.key || dat.data.key })
       }
     })
   }
@@ -359,18 +359,18 @@ function HexContent () {
   }
 }
 
-function errorRow (err) {
-  var errorHexIcon = icon('hexagon-down', {
+function errorRow (err, emit, deleteButton) {
+  var errorHexIcon = icon('hexagon-x', {
     class: 'w2 color-red'
   })
   return html`
-    <tr>
+    <tr class="bg-yellow-disabled">
       <td class="cell-1">
         <div class="w2 center">
           ${errorHexIcon}
         </div>
       </td>
-      <td class="cell-2" colspan="5">
+      <td class="cell-2" colspan="4">
         <div class="cell-truncate color-red">
           <h2 class="f6 f5-l normal">
             Error
@@ -381,6 +381,9 @@ function errorRow (err) {
         </div>
       </td>
       <td class="cell-6">
+        <div class="flex justify-end ${iconStyles}">
+          ${deleteButton.render({ dat: err, emit })}
+        </div>
       </td>
     </tr>
   `
