d2abd11e8fa91c9a372cda0152cd79bb6d6cdda2
Kristina Schneider <me@kristinaschneider.com>
2017-06-15 11:21:28 +0200

    add styled placeholder for menu bar item #429

--- elements/status-bar.js
+++ elements/status-bar.js
@@ -4,12 +4,15 @@ const microcomponent = require('microcomponent')
 const bytes = require('prettier-bytes')
 const html = require('choo/html')
 const css = require('sheetify')
+const button = require('./button')
+const icon = require('./icon')
 
 const style = css`
   :host {
     position: absolute;
     bottom: 0;
     width: 100%;
+    display: flex;
     padding: .5rem 1rem;
     background-color: var(--color-neutral-04);
     color: var(--color-neutral-60);
@@ -28,8 +31,12 @@ module.exports = function () {
     const { up, down } = this.props
     return html`
       <div id="status-bar" class=${style}>
-        <span class="f7 mr3">Download: ${bytes(down)}/s</span>
-        <span class="f7">Upload: ${bytes(up)}/s</span>
+        ${button.icon('Settings', {
+          icon: icon('gear', { class: 'w1' }),
+          class: 'pr3 color-neutral-50 hover-color-neutral-70'
+        })}
+        <span class="f7 pa1 mr3">Download: ${bytes(down)}/s</span>
+        <span class="f7 pa1">Upload: ${bytes(up)}/s</span>
       </div>
     `
   }
