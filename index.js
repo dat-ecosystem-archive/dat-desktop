a1f70547d43990f0fa6e7edbed25f03b3995ca55
Julian Gruber <julian@juliangruber.com>
2018-01-29 16:21:53 +0100

    add back window configuration. closes #474

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 'use strict'
 
 const { app, BrowserWindow } = require('electron')
+const { neutral } = require('dat-colors')
 
 let win
 
 app.on('ready', () => {
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    titleBarStyle: 'hidden-inset',
+    minWidth: 640,
+    minHeight: 395,
+    backgroundColor: neutral
+  })
   win.loadURL(`file://${__dirname}/index.html`)
   win.webContents.openDevTools()
 })
