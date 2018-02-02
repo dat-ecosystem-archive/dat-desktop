2430bae582a1b5e2ccc3f10c5cc109b293361310
Ashok <coderatlabs@gmail.com>
2018-02-02 12:22:54 +0530

    add keydown ev listener to intro

--- app/components/intro.js
+++ app/components/intro.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import styled from 'styled-components'
 import { Green as GreenButton, Plain as PlainButton } from './button'
 
@@ -65,76 +65,99 @@ const Dots = ({ screen }) => (
   </StyledDots>
 )
 
-const IntroScreen = ({ show, screen, openHomepage, next, hide }) => {
-  if (!show) return (
-    <Fragment>
-      <div></div>
-    </Fragment>
-  )
+class IntroScreen extends Component {
+  constructor (props) {
+    super(props)
+    this.onkeydown = this.onkeydown.bind(this)
+  }
 
-  return (
-    <Intro>
-      <Image src={'./assets/intro-'+(screen + 1)+'.svg'} alt='' className='absolute' />
-      <Content>
-        {{
-          1: (
-            <p className='mw5 f4'>
-              Hey there! This is a Dat.
-            </p>
-          ),
-          2: (
-            <p className='mw5 f4'>
-              Think of it as a folder – with some magic.
-            </p>
-          ),
-          3: (
-            <p className='mw5 f4'>
-              You can turn any folder on your computer into a Dat.
-            </p>
-          ),
-          4: (
-            <p className='mw5 f4'>
-              Dats can be easily shared.
-              Just copy the unique dat link and securely share it.
-            </p>
-          ),
-          5: (
-            <p className='mw5 f4'>
-              You can also import existing Dats.
-              Check out <a href='https://datproject.org/' className='color-green-disabled hover-color-green' onClick={() => openHomepage()}>datproject.org</a> to explore open datasets.
-            </p>
-          )
-        }[screen]}
-      </Content>
-      { screen === 1 
-          ? (
-            <GreenButton className="mt2 mb5 relative" onClick={() => next(screen)}>
-              Get Started
-            </GreenButton>
-          )
-          : (
-            <Footer>
-              <PlainButton onClick={() => hide()}>
-                Skip Intro
-              </PlainButton>
-              <Dots screen={screen} />
-              { screen < 5 
-                  ? (
-                    <GreenButton onClick={() => next(screen)}>
-                      Next
-                    </GreenButton>
-                  )
-                  : (
-                    <GreenButton onClick={() => hide()}>
-                      Done
-                    </GreenButton>
-                  )
-              }
-            </Footer>
-          )
-      }
-    </Intro>
-  )
+  onkeydown (ev) {
+    if (ev.code !== 'Escape') return
+    window.removeEventListener('keydown', onkeydown)
+    this.props.hide()
+  }
+
+  componentWillMount () {
+    window.addEventListener('keydown', this.onkeydown)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('keydown', this.onkeydown)
+  }
+
+  render () {
+    const { show, screen, hide, next, onHomepage } = this.props
+
+    if (!show) return (
+      <Fragment>
+        <div></div>
+      </Fragment>
+    )
+
+    return (
+      <Intro>
+        <Image src={'./assets/intro-'+(screen + 1)+'.svg'} alt='' className='absolute' />
+        <Content>
+          {{
+            1: (
+              <p className='mw5 f4'>
+                Hey there! This is a Dat.
+              </p>
+            ),
+            2: (
+              <p className='mw5 f4'>
+                Think of it as a folder – with some magic.
+              </p>
+            ),
+            3: (
+              <p className='mw5 f4'>
+                You can turn any folder on your computer into a Dat.
+              </p>
+            ),
+            4: (
+              <p className='mw5 f4'>
+                Dats can be easily shared.
+                Just copy the unique dat link and securely share it.
+              </p>
+            ),
+            5: (
+              <p className='mw5 f4'>
+                You can also import existing Dats.
+                Check out <a href='https://datproject.org/' className='color-green-disabled hover-color-green' onClick={() => openHomepage()}>datproject.org</a> to explore open datasets.
+              </p>
+            )
+          }[screen]}
+        </Content>
+        { screen === 1 
+            ? (
+              <GreenButton className="mt2 mb5 relative" onClick={() => next(screen)}>
+                Get Started
+              </GreenButton>
+            )
+            : (
+              <Footer>
+                <PlainButton onClick={() => hide()}>
+                  Skip Intro
+                </PlainButton>
+                <Dots screen={screen} />
+                { screen < 5 
+                    ? (
+                      <GreenButton onClick={() => next(screen)}>
+                        Next
+                      </GreenButton>
+                    )
+                    : (
+                      <GreenButton onClick={() => hide()}>
+                        Done
+                      </GreenButton>
+                    )
+                }
+              </Footer>
+            )
+        }
+      </Intro>
+    )
+  }
 }
 
 export default IntroScreen
