023911ccc9164601e0c8a7927ffb80ab7f106cc5
Julian Gruber <julian@juliangruber.com>
2018-02-02 12:18:29 +0100

    add Dat Doctor. closes #478 (#487)
    * add Dat Doctor. closes #478
    
    * prettier

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 'use strict'
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, shell, Menu } = require('electron')
 const { neutral } = require('dat-colors')
+const defaultMenu = require('electron-default-menu')
+const doctor = require('dat-doctor')
+const { Writable } = require('stream')
+
+const menu = defaultMenu(app, shell)
+menu[menu.length - 1].submenu.push({
+  label: 'Doctor',
+  click: () => {
+    win.webContents.openDevTools({ mode: 'detach' })
+    const out = Writable({
+      write (chunk, env, done) {
+        if (win) win.webContents.send('log', chunk.toString())
+        done()
+      }
+    })
+    doctor({ out })
+  }
+})
 
 let win
 
@@ -16,6 +34,7 @@ app.on('ready', () => {
   })
   win.loadURL(`file://${__dirname}/index.html`)
   win.webContents.openDevTools()
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menu))
 })
 
 app.on('window-all-closed', () => app.quit())
