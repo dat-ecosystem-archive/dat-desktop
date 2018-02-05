955dcc408123a0b88b8c69e28eb1321e1b70ffdb
Julian Gruber <julian@juliangruber.com>
2018-02-05 13:36:56 +0100

    fix merge error

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,10 +1,9 @@
 'use strict'
 
 import Dat from 'dat-node'
-import { shell, clipboard } from 'electron'
+import { shell, clipboard, remote } from 'electron'
 import { encode } from 'dat-encoding'
 import { homedir } from 'os'
-import { clipboard, remote } from 'electron'
 import mirror from 'mirror-folder'
 import fs from 'fs'
 import promisify from 'util-promisify'
