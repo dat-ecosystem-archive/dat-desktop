45fcdf480b8b3a0f5b6b6789f68966f91b6030be
AtuyL <atuyl.jp@gmail.com>
2018-10-12 02:34:09 +0900

    fixed: From inspect view back to dat view still showed inspect view on screen bottom.

--- app/containers/inspect.js
+++ app/containers/inspect.js
@@ -1,11 +1,12 @@
 'use strict'
 
+import SCREEN from '../consts/screen'
 import Inspect from '../components/inspect'
 import { closeInspectDat } from '../actions'
 import { connect } from 'react-redux'
 
 const mapStateToProps = state => ({
-  dat: state.dats[state.inspect.key]
+  dat: state.screen === SCREEN.INSPECT ? state.dats[state.inspect.key] : null
 })
 
 const mapDispatchToProps = dispatch => ({
