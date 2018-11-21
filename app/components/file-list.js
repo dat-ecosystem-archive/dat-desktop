4d20420554e2c0f5062309e09baf505aa09e41b2
AtuyL <atuyl.jp@gmail.com>
2018-11-21 09:28:31 +0900

    improved: file-list changed styling for style-guideline.

--- app/components/file-list.js
+++ app/components/file-list.js
@@ -4,52 +4,41 @@ import React from 'react'
 import styled from 'styled-components'
 import bytes from 'prettier-bytes'
 
-const FileListContainer = styled.div`
-  :host {
-    min-height: 5rem;
-  }
-`
-
 const FileListTable = styled.table`
-  :host {
-    td {
-      padding: 0.25rem 0.5rem;
-    }
-    tr:odd td {
-      background-color: var(--color-neutral-04);
-    }
+  width: 100%;
+  border-collapse: collapse;
+  tr:odd td {
+    background-color: var(--color-neutral-04);
+  }
+  td {
+    border: 0;
+  }
+  td:last-child {
+    width: 4rem;
+    text-align: right;
   }
 `
 
-const List = ({ dat }) => (
-  <FileListContainer>
-    {dat && dat.files && dat.files.length ? (
-      <FileListTable className='w-100 f7 f6-l '>
-        <tbody>
-          {dat.files.map(file => {
-            const size =
-              Number(file.size) === file.size && file.isFile
-                ? bytes(file.size)
-                : ''
-            return (
-              <tr key={file.path}>
-                <td className='truncate mw5'>{file.path}</td>
-                <td>{size}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </FileListTable>
-    ) : (
-      <div className='f7 f6-l pa2'>N/A</div>
-    )}
-  </FileListContainer>
-)
-
-const FileList = ({ dat }) => (
-  <FileListContainer className='flex-auto bg-white mb2 mw6'>
-    <List dat={dat} />
-  </FileListContainer>
-)
+const FileList = ({ dat, fallback = null }) => {
+  if (!dat || !dat.files || !dat.files.length) return fallback
+  return (
+    <FileListTable className='w-100 f7 f6-l'>
+      <tbody>
+        {dat.files.map(file => {
+          const size =
+            Number(file.size) === file.size && file.isFile
+              ? bytes(file.size)
+              : ''
+          return (
+            <tr key={file.path}>
+              <td className='truncate'>{file.path}</td>
+              <td>{size}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </FileListTable>
+  )
+}
 
 export default FileList
